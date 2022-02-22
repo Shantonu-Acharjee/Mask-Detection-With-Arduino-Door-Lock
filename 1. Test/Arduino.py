a7b1858fee5ce8aecccc949bd9164f5cc5de627c
Shantonu Acharjee <shantonuacharjee@gmail.com>
2022-02-23 00:43:28 +0600

    All Done

---
+++ 1. Test/Arduino.py
@@ -0,0 +1,21 @@
+from pyfirmata import ArduinoMega
+import time
+import keyboard
+port = 'COM3'
+board = ArduinoMega(port)
+
+
+
+
+
+def ArduinoState(valu):
+    print(valu)
+        
+    if valu == 1:
+        board.digital[13].write(1)
+            
+    else:
+        board.digital[13].write(0)
+        
+
+
